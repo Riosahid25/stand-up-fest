5caae5ed9b7780a48dfbd697bec61dac15e29d40
Riosahid25 <riosahid25@gmail.com>
2023-04-03 13:51:01 +0700

    Benerin pada section registrasi layout, text-size, dan validation form

---
+++ js/registrasi_tiket.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $("select.op_category")
+    .change(function () {
+      $(this)
+        .find("option:selected")
+        .each(function () {
+          var optionValue = $(this).attr("value");
+          if (optionValue) {
+            $(".box")
+              .not("." + optionValue)
+              .hide();
+            $("." + optionValue).show();
+          } else {
+            $(".box").hide();
+          }
+        });
+    })
+    .change();
+});
